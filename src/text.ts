bad288fb01206e9c4e73480dccd40b9e812b9ba1
Crais <tomislav@tmweb.dev>
2024-06-12 11:38:15 +0200

    Update got version.

--- src/text.ts
+++ src/text.ts
@@ -2,13 +2,13 @@
 
 /*const got = (...args) => import('got').then(({default: got}) => got(...args));*/
 
-/*let got;
+let got;
 
 (async () => {
     got = await import('got').then(got => got.default)
-})().catch(console.error);*/
+})().catch(console.error);
 
-import axios from "axios";
+// import axios from "axios";
 import intoStream from 'into-stream';
 import isStream from 'is-stream';
 import getStream from 'get-stream';
@@ -20,18 +20,18 @@ const URL = 'http://localhost:9998/tika';
  * @param input File to extract text from
  * @return Extracted text
  */
-export async function extract(input: Buffer | string | Promise<string> = ''): Promise<string> {
+export function extract(input: Buffer | string | Promise<string> = ''): Promise<string> {
   const fileStream = isStream(input) ? input : intoStream(input);
-  /*const tikaStream = got.stream.put(URL, {
+  const tikaStream = got.stream.put(URL, {
     headers: {Accept: 'text/plain'},
-  });*/
+  });
 
-    const response = await axios.put(URL, {
+    /*const response = await axios.put(URL, {
         headers: {Accept: 'text/plain'},
         responseType: 'stream'
     });
 
-    const tikaStream = response.data;
+    const tikaStream = response.data;*/
 
   return getStream(fileStream.pipe(tikaStream));
 }
