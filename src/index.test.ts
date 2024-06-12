67799467d9e26a2d30bb339eac1d1c58c1bf0348
Crais <tomislav@tmweb.dev>
2024-06-12 11:15:38 +0200

    Update got version.

--- src/index.test.ts
+++
@@ -1,6 +0,0 @@
-import tte from './index';
-
-it('should expose extract and startServer functions', () => {
-  expect(tte.extract).toBeInstanceOf(Function);
-  expect(tte.startServer).toBeInstanceOf(Function);
-});
